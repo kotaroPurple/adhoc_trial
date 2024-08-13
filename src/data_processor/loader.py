ec18ad4b56597781580e324b1078fad712010726
kotaro <11015194+kotaroPurple@users.noreply.github.com>
2024-08-13 14:06:07 +0900

    add data loader

---
+++ src/data_processor/loader.py
@@ -0,0 +1,12 @@
+
+from pathlib import Path
+import pandas as pd
+import pandera as pa
+from pandera.typing import DataFrame
+from .schema import SalesDataSchema
+
+
+@pa.check_types
+def load_sales_data(filepath: Path) -> DataFrame[SalesDataSchema]:
+    df = pd.read_csv(filepath)
+    return df
