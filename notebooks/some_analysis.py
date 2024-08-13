c6dd9556e0c3eb085e068fb9e7db7e00045dcaeb
kotaro <11015194+kotaroPurple@users.noreply.github.com>
2024-08-13 14:06:17 +0900

    add adhoc analysis script

---
+++ notebooks/some_analysis.py
@@ -0,0 +1,14 @@
+
+# %%
+from pathlib import Path
+
+from data_processor.loader import load_sales_data
+
+# %%
+REPO_ROOT = Path('../')
+
+# %%
+df = load_sales_data(REPO_ROOT / "data" / "processed" / "sales_data.csv")
+print(df)
+
+# %%
