f42e886a3c33f9a8b0e5009d65feb61f22ed820a
kotaroPurple <kotaroPurple@users.noreply.github.com>
2024-08-16 23:38:09 +0900

    pytest trial

---
+++ tests/data_processor/test_process.py
@@ -0,0 +1,10 @@
+
+from data_processor.process import process_sales_data
+from data_processor.schema import SalesDataSchema
+
+
+def test_dummy():
+    a = 1
+    b = 1
+    print('\ntest_dummy')
+    assert a == b
